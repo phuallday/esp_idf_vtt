8305403e0a3605971572974cad0dc01e4aac1421
phuallday <phualldaykute@gmail.com>
2024-02-21 14:59:45 +0700

    update-character-lcd

--- main/main.c
+++ main/main.c
@@ -2,7 +2,7 @@
 #include "esp_event.h"
 #include "event_source.h"
 #include "character_lcd.h"
-int min = 0, now = 0, max = 0;
+
 void app_main(void) {
     esp_event_loop_create_default();
     lcd_init();
