a2def8527b2b0fa97fad74ab8ee8310d03e2fa50
phuallday <phualldaykute@gmail.com>
2024-02-22 14:19:49 +0700

    add reconnect wifi forever set ESP_MAXIMUM_RETRY = -1

--- main/connection.c
+++ main/connection.c
@@ -75,13 +75,19 @@ static void event_handler(void *arg, esp_event_base_t event_base,
         esp_wifi_connect();
     }
     else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < ESP_MAXIMUM_RETRY) {
+        if (ESP_MAXIMUM_RETRY == -1) {
             esp_wifi_connect();
-            s_retry_num++;
             ESP_LOGI(TAG, "retry to connect to the AP");
         }
         else {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+            if (s_retry_num < ESP_MAXIMUM_RETRY) {
+                esp_wifi_connect();
+                s_retry_num++;
+                ESP_LOGI(TAG, "retry to connect to the AP");
+            }
+            else {
+                xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+            }
         }
         ESP_LOGI(TAG, "connect to the AP fail");
     }
@@ -98,7 +104,7 @@ void wifi_init_sta(void) {
 
     ESP_ERROR_CHECK(esp_netif_init());
 
-    //ESP_ERROR_CHECK(esp_event_loop_create_default());
+    // ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_sta();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
